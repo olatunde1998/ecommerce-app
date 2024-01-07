04a4f8da96a27f5ce55aefbbd642a4cb844878ef
geodevcode <geodevinstitute@gmail.com>
2024-01-07 01:50:23 +0100

    footer links added

---
+++ components/footer/FooterLinks.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const FooterLinks = () => {
+  return (
+    <div>
+      <h2 className="uppercase mb-4">Categories</h2>
+      <p>Hot deals</p>
+      <p>Laptops</p>
+      <p>SmartPhones</p>
+      <p>Camera</p>
+      <p>Accessories</p>
+    </div>
+  );
+};
+
+export default FooterLinks;
