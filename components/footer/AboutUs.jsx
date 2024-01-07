536aff824be7f9b124920e633425f5de2c9fae98
geodevcode <geodevinstitute@gmail.com>
2024-01-07 01:49:05 +0100

    aboutus in foooter added

---
+++ components/footer/AboutUs.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <div className="">
+      <h2 className="uppercase">About Us</h2>
+      <p className="max-w-[400px] my-4">
+        Lorem ipsum dolor sit amet consectet vulputate egestas malesuada potenti
+        porta consequat aliquet. Facilisi pretium at massa nulla.
+      </p>
+        <p>icon-one <span className="ml-4">174 Stonecoal Road</span></p> 
+        <p>icon-two <span className="ml-4">+23481009088</span></p> 
+        <p>icon-three <span className="ml-4">email@email.com</span></p> 
+    </div>
+  );
+};
+
+export default AboutUs;
