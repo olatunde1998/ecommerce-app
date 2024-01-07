bc16f456439b654f15be4e1d7a744b5eee10bff8
geodevcode <geodevinstitute@gmail.com>
2024-01-07 01:49:24 +0100

    footer section added

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import AboutUs from "./AboutUs";
+import FooterLinks from "./FooterLinks";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <div className="bg-[#15161D] p-6 text-white w-full">
+        <div className="md:flex md:gap-4 max-w-[1150px] justify-between  mx-auto">
+          <AboutUs />
+          <FooterLinks />
+          <FooterLinks />
+          <FooterLinks />
+        </div>
+      </div>
+      <p className="bg-[#15161D] p-4 text-white w-full text-center opacity-95">
+        copyright 2023 All right Reserved | This template is made by geodevcodes
+      </p>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
